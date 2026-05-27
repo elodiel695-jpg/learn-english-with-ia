62f31a6741f04d3f9f91f3a232f968054ffd0b64
Elodie Ledent <elodiel695@gmail.com>
2026-05-27 07:56:04 +0200

    feat: création de SessionRepository

--- learn_english_with_ia/src/main/java/com/example/learn_english_with_ia/model/Report.java
+++ learn_english_with_ia/src/main/java/com/example/learn_english_with_ia/model/Report.java
@@ -1,8 +1,16 @@
 package com.example.learn_english_with_ia.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Report {
 
     @Id
